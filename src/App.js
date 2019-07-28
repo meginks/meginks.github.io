64109479d8164e08704b8518a1f479bf658babe0
meginks <meganjones.dev@gmail.com>
2019-07-28 14:49:46 -0500

    fixed for mobile

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div id="app">
       <div className="definition">
-      <div> <h1 className="fontFellEnglish" >Megan Jones </h1></div>
+      <div> <h1 className="fontFellEnglish name" >Megan Jones </h1></div>
       
       <div className="offset-def">
       <p className="fontAlegreya"><strong>|ˈmɛɡən ˈdʒoʊnz |</strong> <em className="fontAlegreya">noun</em> <br></br> <sup>1</sup>Software developer.  <sup>2</sup>Writer.  <sup>3</sup>Human. </p> 

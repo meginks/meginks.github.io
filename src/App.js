17b4e02c1ce28134a08126b663645e529db060bf
meginks <meganjones.dev@gmail.com>
2019-07-28 14:22:33 -0500

    style shift

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import Home from './components/Home';
-
 import './App.css';
 import {Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-     <Route exact path="/" component={Home}/>
+    <div id="app">
+      <div className="definition">
+      <div> <h1 className="fontFellEnglish" >Megan Jones </h1></div>
+      
+      <div className="offset-def">
+      <p><strong className="fontAlegreya">|ˈmɛɡən ˈdʒoʊnz |</strong> <em className="fontAlegreya">noun</em> <br></br> <sup>1</sup>Software developer.  <sup>2</sup>Writer.  <sup>3</sup>Human. </p> 
+      </div>
+      </div>
     </div>
   );
 }

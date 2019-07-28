6a938be721a08424939f06b7a17a513cd62f1bf5
meginks <meganjones.dev@gmail.com>
2019-07-28 14:24:20 -0500

    fixed font

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <div> <h1 className="fontFellEnglish" >Megan Jones </h1></div>
       
       <div className="offset-def">
-      <p><strong className="fontAlegreya">|ˈmɛɡən ˈdʒoʊnz |</strong> <em className="fontAlegreya">noun</em> <br></br> <sup>1</sup>Software developer.  <sup>2</sup>Writer.  <sup>3</sup>Human. </p> 
+      <p className="fontAlegreya"><strong>|ˈmɛɡən ˈdʒoʊnz |</strong> <em className="fontAlegreya">noun</em> <br></br> <sup>1</sup>Software developer.  <sup>2</sup>Writer.  <sup>3</sup>Human. </p> 
       </div>
       </div>
     </div>

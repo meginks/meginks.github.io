4cd072d7d8091cb55cc051677733d98dba39d8aa
meginks <meganjones.dev@gmail.com>
2019-08-04 15:27:08 -0500

    fixed definition styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,7 +13,7 @@ function App() {
         <div> <h1 className="fontFellEnglish name" >Megan Jones </h1></div>
         
         <div className="offset-def">
-        <p className="fontAlegreya"><strong>|ˈmɛɡən ˈdʒoʊnz |</strong> <em className="fontAlegreya">noun</em> <br></br> <sup>1</sup>Software developer.  <sup>2</sup>Writer.  <sup>3</sup>Human. </p> 
+        <p className="fontAlegreya def"><strong>|ˈmɛɡən ˈdʒoʊnz |</strong> <em className="fontAlegreya">noun</em> <br></br> <sup>1</sup>Software developer.  <sup>2</sup>Writer.  <sup>3</sup>Human. </p> 
         </div>
         </div>
         <img src={ink7} alt="ink spot" className="splatter"/>
